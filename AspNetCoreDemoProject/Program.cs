fbedc4c20a8b58f3df86335a36fa538abf951326
serkanduman <serkanduman03@hotmail.com>
2023-03-09 20:28:33 +0300

    404 Not Found sayfası oluşturuldu.

--- AspNetCoreDemoProject/Program.cs
+++ AspNetCoreDemoProject/Program.cs
@@ -19,6 +19,8 @@ namespace AspNetCoreDemoProject
                 app.UseHsts();
             }
 
+            app.UseStatusCodePagesWithReExecute("/ErrorPage/Error1", "?code={0}");
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
