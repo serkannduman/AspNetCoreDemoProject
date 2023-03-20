80e86b1bfa810d0540c4a0d2ff66b4f1cf15f642
serkanduman <serkanduman03@hotmail.com>
2023-03-20 10:41:27 +0300

    Admin Paneli giriş & kategori listeleme

---
+++ AspNetCoreDemoProject/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreDemoProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult AdminNavbarPartial()
+        {
+            return PartialView();
+        }
+    }
+}
--- AspNetCoreDemoProject/Program.cs
+++ AspNetCoreDemoProject/Program.cs
@@ -14,7 +14,7 @@ namespace AspNetCoreDemoProject
             // Add services to the container.
             builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
-            
+
 
             builder.Services.AddMvc(config =>   //Tüm projeye eriþim engeli koyuyor , Login controllerda index metoduna AllowAnonymous attribute'i koyduk giriþ yaptýktan sonra diðer actionlara eriþim engelini kaldýrýcaz 
             {
@@ -49,15 +49,24 @@ namespace AspNetCoreDemoProject
 
             app.UseAuthentication();
 
-            
+
 
             app.UseRouting();
 
             app.UseAuthorization();
 
             app.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+
+            app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+
+
 
             app.Run();
         }
